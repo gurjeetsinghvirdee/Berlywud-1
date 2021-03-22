71e96f2e24c4e26d522bcd3b15246ace1d793fac
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2021-03-22 13:47:35 +0530

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema(
     {
-        url : {type:String, required:true},
-        brand:{type:String, required:true},
-        title:{type:String, required:true, unique: true},
-        description : {type:String, required:true},
-        origprice : {type:Number,required:true},
-        decantprice : {"2ml":{type:Number,required:true},"5ml":{type:Number,required:true},"10ml":{type:Number,required:true},"30ml":{type:Number,required:true},"Retail":{type:Number,required:true}},
+        url: {type:String, required:true},
+        brand: {type:String, required:true},
+        title: {type:String, required:true, unique: true},
+        description: {type:String, required:true},
+        origprice: {type:Number,required:true},
+        decantprice: {"2ml":{type:Number,required:true},"5ml":{type:Number,required:true},"10ml":{type:Number,required:true},"30ml":{type:Number,required:true},"Retail":{type:Number,required:true}},
         gender: {type:String, required:true},
-        notes:{Topnotes:[{type:String, required:true},{type:String, required:true},{type:String, required:true}],Middlenotes:[{type:String, required:true},{type:String, required:true},{type:String, required:true}],Basenotes:[{type:String, required:true},{type:String, required:true},{type:String, required:true}]},
-        launch:{type:Number,required:true},
-        concentration:{type:String, required:true},
-        rating:{type:Number,required:true},
+        notes: {Topnotes:[{type:String, required:true},{type:String, required:true},{type:String, required:true}],Middlenotes:[{type:String, required:true},{type:String, required:true},{type:String, required:true}],Basenotes:[{type:String, required:true},{type:String, required:true},{type:String, required:true}]},
+        launch: {type:Number,required:true},
+        concentration: {type:String, required:true},
+        rating: {type:Number,required:true},
         reviews: {type:Number,required:true},
         stockcount:{type:Number,required:true}
     },
